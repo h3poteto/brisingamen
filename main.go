c4f9acc6edf06e42ef2f8f6413b9ce96be153b31
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2016-03-04 16:39:26 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path/filepath"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v2"
+)
+
+type SlackMessage struct {
+	Text      string `json:"text"`       //投稿内容
+	Username  string `json:"username"`   //投稿者名 or Bot名（存在しなくてOK）
+	IconEmoji string `json:"icon_emoji"` //アイコン絵文字
+	IconURL   string `json:"icon_url"`   //アイコンURL（icon_emojiが存在する場合は、適応されない）
+	Channel   string `json:"channel"`    //#部屋名
+}
+
+func main() {
+	client, err := initClient()
+	if err != nil {
+		panic(err)
+	}
+
+	labels := []string{}
+	for _, l := range Config("labels").([]interface{}) {
+		labels = append(labels, l.(string))
+	}
+
+	listOptions := &github.IssueListByRepoOptions{
+		State:  "open",
+		Labels: labels,
+	}
+
+	repo := Config("repository").(map[interface{}]interface{})
+	issues, _, err := client.Issues.ListByRepo(repo["owner"].(string), repo["repo"].(string), listOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	// issueだとpullRequestも含めて全て取れるので、pullRequestに限定する
+	pullRequests := []github.Issue{}
+	for _, issue := range issues {
+		if isPullRequest(issue) {
+			pullRequests = append(pullRequests, issue)
+		}
+	}
+	fmt.Printf("pullRequest: %v\n", pullRequests)
+
+	slackPost(pullRequests)
+}
+
+func initClient() (c *github.Client, e error) {
+	token := Config("access_token")
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token.(string)},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+	client := github.NewClient(tc)
+	return client, nil
+}
+
+func Config(elem string) interface{} {
+	path := filepath.Join("./", "settings.yml")
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		panic(err)
+	}
+	return m[elem]
+}
+
+func isPullRequest(issue github.Issue) bool {
+	if issue.PullRequestLinks == nil {
+		return false
+	}
+	return true
+}
+
+func slackPost(pullRequest []github.Issue) {
+	slackURL := Config("slack_url")
+	channelName := Config("channel")
+
+	params, _ := json.Marshal(SlackMessage{
+		"Hello World",
+		"PleaseReview",
+		":ghost:",
+		"",
+		channelName.(string)})
+
+	resp, _ := http.PostForm(
+		slackURL.(string),
+		url.Values{"payload": {string(params)}},
+	)
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	println(string(body))
+
+}
